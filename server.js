aa9ea490b4e5b1cc8687da44da827700896f7a44
Laura Daugherty <lauradaugherty6@gmail.com>
2019-08-21 12:46:08 -0700

    Boilerplate

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const AuthRouter = require('./auth/auth_router.js');
+const UsersRouter = require('./users/users_router.js');
+const PositionsRouter = require('./positions/positions_router.js');
+const DepartmentsRouter = require('./departments/departments_router.js');
+
+
+const server = express();
+
+server.use(express.json());
+
+server.use('/api/auth', AuthRouter)
+server.use('/api/users', UsersRouter)
+server.use('/api/positions', PositionsRouter)
+server.use('/api/departments', DepartmentsRouter)
+
+server.get('/', (req, res) => {
+  res.json({ api: 'up', session: req.session })
+});
+
+module.exports = server;
